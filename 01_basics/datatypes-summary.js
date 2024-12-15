293e3ace0e023df05befd89ee011560c1c54b972
StillWater2005 <165381369+StillWater2005@users.noreply.github.com>
2024-12-15 05:24:58 +0000

    datatypes_summary video file

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,20 @@
+const id1 = Symbol("123");
+const id2 = Symbol("123");
+console.log(id1 === id2)
+
+items = ["apple", "mango", "orange"]
+console.log(items)
+console.log(typeof items)
+list = {
+ fruit1: "apple",
+ fruit2: "mango",
+ fruit3: "orange"   
+}
+console.log(list)
+console.log(typeof list)
+
+let my_function = function(){
+    console.log("Hello World!\n")
+}
+my_function()
+console.log(typeof my_function)
