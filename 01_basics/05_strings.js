8920a4b3c0d09de73d89110e39f9bbbc4399f8f9
StillWater2005 <165381369+StillWater2005@users.noreply.github.com>
2024-12-15 08:02:31 +0000

    strings

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,10 @@
+const name = "  Priyanshu Pareek  "
+const age = 19
+console.log(`Hello ${name} your age is ${age}`)
+console.log(name.slice(0,10))
+console.log(name.toUpperCase())
+console.log(name.replace(" " , "-"))
+console.log(name.trim())
+console.log(name.includes("Pareek"))
+console.log(name.includes("Sharma"))
+console.log(name.trim().toUpperCase().split(" ", 2))
