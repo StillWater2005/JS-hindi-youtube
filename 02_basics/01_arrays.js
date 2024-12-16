ae9087e0221f06dfe299be846cb61656b9e83373
StillWater2005 <165381369+StillWater2005@users.noreply.github.com>
2024-12-16 04:21:59 +0000

    01_arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,26 @@
+//Creation of Arrays
+const nums = [1,2,3,4,5]
+const fruits = ["apple", "orange", "banana", "mango", "grapes"]
+const myArr = new Array(1,2,"hello", "world")
+
+//Array methods
+
+// pop and push
+
+// nums.push(6)
+// console.log(nums)
+// fruits.pop()
+// console.log(fruits)
+
+//shift and unshift(not very useful have to shift whole array)
+
+// nums.unshift(0)
+// console.log(nums)
+// nums.shift()
+// console.log(nums)
+
+//includes and indexof methods
+
+//join method
+
+//slice and splice methods(splice manipulates original array)
